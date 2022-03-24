79118427cc9602cfdbc6f0b2310c4b0a2cb6a316
We Care Insurance <69594979+wecareinsurance@users.noreply.github.com>
2022-03-24 19:18:06 -0400

    HTML Skeleton for Language Selection (welcome.html)

--- js/renderTips.js
+++ js/renderTips.js
@@ -1,74 +1,74 @@
 export const renderTips = () => {
-    const tips = [
-        {
-            type: "Tip1",
-            tip: "Tip/Text .. ", 
-        },
-        {
-            type: "Tip2",
-            tip: "Tip/Text ..",
-        },
-        {
-            type: "Tip3",
-            tip: "Tip/Text",
-        },
-        {
-            type: "Tip4",
-            tip: "Tip/Text ..",
-        },
-        {
-            type: "Tip5",
-            tip: "Tip/Text ",
-        },
-        {
-            type: "Tip6",
-            tip: "Tip/Text "
-        },  
-      
-    ];
-    var tipsCopy = tips.slice();
+  const tips = [
+      {
+          type: "Tip1",
+          tip: "Tip/Text .. ", 
+      },
+      {
+          type: "Tip2",
+          tip: "Tip/Text ..",
+      },
+      {
+          type: "Tip3",
+          tip: "Tip/Text",
+      },
+      {
+          type: "Tip4",
+          tip: "Tip/Text ..",
+      },
+      {
+          type: "Tip5",
+          tip: "Tip/Text ",
+      },
+      {
+          type: "Tip6",
+          tip: "Tip/Text "
+      },  
+    
+  ];
+  var tipsCopy = tips.slice();
 
-    const chooseTip = () => {
-      if(tipsCopy.length == 0) {
-        tipsCopy = tips.slice();
-      }
-      var index = Math.floor(Math.random() * tipsCopy.length);
-      var tip = tipsCopy[index];
-      tipsCopy.splice(index, 1);
-
-      let tipSpan = document.createElement("span");
-      let tipType = document.createElement("STRONG");
-      let tipText = document.createElement("span");
-      tipType.innerHTML = "🛒 " + tip.type + ": ";
-      tipText.innerHTML = tip.tip;
-      tipSpan.appendChild(tipType);
-      tipSpan.appendChild(tipText);
-      return tipSpan;
-    };
-
-    const tipSequence = () => {
-      var newTip = chooseTip();
-      setTimeout(function() {
-        try {
-          var tipElement = document.getElementById("insurance-tip");
-          tipElement.classList.add("hide");
-          setTimeout(function() {
-            // tipElement.innerHTML = newTip;
-            tipElement.innerHTML = "";
-            tipElement.appendChild(newTip);
-            tipElement.classList.remove("hide");
-            tipSequence();
-          }, 100);
-        }
-        catch(error) {
-          return;
-        }
-      }, 1000);
+  const chooseTip = () => {
+    if(tipsCopy.length == 0) {
+      tipsCopy = tips.slice();
     }
+    var index = Math.floor(Math.random() * tipsCopy.length);
+    var tip = tipsCopy[index];
+    tipsCopy.splice(index, 1);
 
-    let newTip = chooseTip();
-    var tipElement = document.getElementById("insurance-tip");
-    tipElement.innerHTML = "";
-    tipElement.appendChild(newTip);
-    tipSequence();
+    let tipSpan = document.createElement("span");
+    let tipType = document.createElement("STRONG");
+    let tipText = document.createElement("span");
+    tipType.innerHTML = "🛒 " + tip.type + ": ";
+    tipText.innerHTML = tip.tip;
+    tipSpan.appendChild(tipType);
+    tipSpan.appendChild(tipText);
+    return tipSpan;
+  };
+
+  const tipSequence = () => {
+    var newTip = chooseTip();
+    setTimeout(function() {
+      try {
+        var tipElement = document.getElementById("tips");
+        tipElement.classList.add("hide");
+        setTimeout(function() {
+          // tipElement.innerHTML = newTip;
+          tipElement.innerHTML = "";
+          tipElement.appendChild(newTip);
+          tipElement.classList.remove("hide");
+          tipSequence();
+        }, 100);
+      }
+      catch(error) {
+        return;
+      }
+    }, 1000);
+  }
+
+  let newTip = chooseTip();
+  var tipElement = document.getElementById("tips");
+  tipElement.innerHTML = "";
+  tipElement.appendChild(newTip);
+  tipSequence();
 }
